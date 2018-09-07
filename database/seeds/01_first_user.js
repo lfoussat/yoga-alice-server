3e1baab8ced25376565e9cf7cafac64c2220b3ed
Louise Foussat <louisefoussat@gmail.com>
2018-09-07 16:49:38 +0200

    knex seeds

---
+++ database/seeds/01_first_user.js
@@ -0,0 +1,19 @@
+
+exports.seed = (knex, Promise) => {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(() => {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          id: 1,
+          created_at: new Date(),
+          username: 'lfoussat',
+          first_name: '',
+          last_name: '',
+          email: 'louisefoussat@gmail.com',
+          password: 'yogatest',
+        }
+      ])
+    })
+}
