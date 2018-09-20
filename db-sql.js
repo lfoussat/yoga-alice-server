339b51af1baf0b3943586a3ac9432fcba12820c4
Louise Foussat <louisefoussat@gmail.com>
2018-09-20 11:16:54 +0200

    create 2 diff get inspiration by id requests for bo & fo

--- db-sql.js
+++ db-sql.js
@@ -9,14 +9,22 @@ const getInspirations = async () => {
   return inspirations.map(camelSnake)
 }
 
-
-const getInspirationById = async id => {
+const getInspirationById = async id => { // get inspiration for front
   const inspiration = await knex
     .select()
     .table('inspirations')
     .where('id', id)
-    .returning('id', 'title', 'small_description', 'description', 'color', 'image_url')
-  return inspiration.camelSnake()
+    .returning('id', 'title', 'small_description', 'description', 'color', 'image_url', 'is_draft')
+  return inspiration.map(camelSnake)[0]
+}
+
+const getInspirationByIdForBO = async id => { // get inspiration for back office
+  const inspiration = await knex
+    .select()
+    .table('inspirations')
+    .where('id', id)
+    .returning('id', 'draft_title', 'draft_small_description', 'draft_description', 'draft_color', 'draft_image_url', 'is_draft', 'modification_date', 'publication_date', 'created_at')
+  return inspiration.map(camelSnake)[0]
 }
 
 const createInspiration = async (params) => {
