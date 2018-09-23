a944fa3d4b3a6a5bf8c45a98dfbc7ebad0d5d7e2
Louise Foussat <louisefoussat@gmail.com>
2018-09-23 17:31:40 +0200

    import local js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const path = require('path')
-
+const auth = require('./auth/local.js')
 const db = require('./db-sql.js')
 
 const app = express()
