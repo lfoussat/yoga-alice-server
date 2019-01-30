d4a0725fd69e43d32be4f47bd06f6f6e278e23fb
Louise Foussat <louisefoussat@gmail.com>
2019-01-30 18:43:25 +0100

    change user seed

--- database/seeds/01_first_user.js
+++ database/seeds/01_first_user.js
@@ -1,7 +1,7 @@
-
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('users')
+    .del()
     .then(() => {
       // Inserts seed entries
       return knex('users').insert([
@@ -13,6 +13,16 @@ exports.seed = (knex, Promise) => {
           last_name: '',
           email: 'louisefoussat@gmail.com',
           password: 'yogatest'
+        },
+        {
+          id: 2,
+          created_at: new Date(),
+          username: 'aliceO',
+          first_name: '',
+          last_name: '',
+          email: 'alice.ollagnon@gmail.com',
+          password:
+            '$2b$09$AHDKLV1a.3NZGqhug37lDeKT6fJJvRlpTa32uQaAnWsD1GmdOfqgq'
         }
       ])
     })
